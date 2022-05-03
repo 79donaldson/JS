51c9aeb6819ad00577a6204835618186d6b5a2b2
Sushko Aleksii <79donaldson@gmail.com>
2022-05-03 21:22:12 +0300

    add new lesson

---
+++ lesson7_1_Let_Const/index.js
@@ -0,0 +1,15 @@
+// for (let i=0; i < 5; i++){
+//     setTimeout(function(){
+//         console.log(i)
+//     }, 2000)
+    
+// }
+    
+
+const HEX = '#AAAAFF'
+
+//HEX = 'color'
+
+document.querySelector('h1').style.color = HEX
+
+console.log(HEX)
---
+++ lesson7_6_строки/index.js
@@ -0,0 +1,17 @@
+const createLink = ({path, name}) => `<a target="_blank", href="${path}">"${name}</a>`
+
+const ul = document.querySelector('ul')
+
+const google = `<li>'${createLink({path: 'https://google.com', name: 'Google'})}</li>`
+
+const elephant = `<li>'${createLink({path: 'https://s-elephant.dp.ua', name: 'S-elephant'})}</li>`
+
+ul.insertAdjacentHTML('afterbegin', google)
+ul.insertAdjacentHTML('afterbegin', elephant)
+
+const strTolog =`
+Hello
+  Hello
+    Hello
+`
+console.log(strTolog)
